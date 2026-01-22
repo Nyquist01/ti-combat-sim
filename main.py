c4f436535ddd87833324e52bb86758eb33eb095a
Brandon Tupou <tupoubrandon@gmail.com>
2026-01-22 01:08:51 +0000

    Add a naive priority strategy for assigning hits

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """
 Calculate the outcome of this space combat:
 
-2 dreadnaughts vs 1 dreadnaught + 1 carrier + 4 fighters vs 2 dreadnaughts
+2 dreadnaughts vs 1 dreadnaught + 1 carrier + 4 fighters
 
 Dreadnaught:
 2 HP
@@ -23,29 +23,36 @@ Pseudo:
 4. Second player assigns hits
 """
 
-
+from abc import ABC
 import random
 
 
-class Ship:
-    def __init__(self):
-        self.hp = 0
+class Ship(ABC):
+    hp: int
+    combat: int
+    priority: int
+    name: str
     
     def take_damage(self):
         self.hp -= 1
-
+        
+    def __repr__(self):
+        return f"<name={self.name} hp={self.hp}>"
+        
 
 class Dreadnaught(Ship):
     def __init__(self):
         self.hp = 2
         self.combat = 5
+        self.priority = 1
         self.name = "dreadnaught"
-
+        
 
 class Fighter(Ship):
     def __init__(self):
         self.hp = 1
         self.combat = 9
+        self.priority = 2
         self.name = "fighter"
 
 
@@ -53,6 +60,7 @@ class Carrier(Ship):
     def __init__(self):
         self.hp = 1
         self.combat = 9
+        self.priority = 3
         self.name = "carrier"
 
 
@@ -60,61 +68,65 @@ def roll_d10() -> int:
     return random.randint(1, 10)
 
 
+def roll_fleet_hits(fleet: list[Ship]) -> int:
+    """Returns the number of hits a fleet produced"""
+    hits = 0
+    for ship in fleet:
+        res = roll_d10()
+        if res >= ship.combat:
+            hits += 1
+    return hits
+
+
+def assign_fleet_hits(hits: int, target_fleet: list[Ship]):
+    """
+    Assigns hits to the target fleet.
+    
+    Assume hit strategy where we want to keep our 'best' ships alive
+    for as long as possible (i.e. dreadnaughts).
+    """
+    for _ in range(hits):
+        if len(target_fleet) == 0:
+            break
+        target_fleet.sort(key=lambda ship: (-ship.priority, -ship.hp))
+        ship_to_hit = target_fleet[0]
+        ship_to_hit.take_damage()
+        if ship_to_hit.hp <= 0:
+            target_fleet.pop(0)
+
+
 def main():
-    fleet_1: list[Ship] = [Dreadnaught(), Dreadnaught()]
-    fleet_2: list[Ship] = [Dreadnaught(), Fighter(), Fighter(), Fighter(), Fighter(), Carrier()]
+    """Classic Monte Carlo simulation"""
+    simulations = 10000
     fleet_1_wins = 0
     fleet_2_wins = 0
+    draws = 0
 
-    for _ in range(10000):
+    for _ in range(simulations):
+        fleet_1: list[Ship] = [Dreadnaught(), Dreadnaught()]
+        fleet_2: list[Ship] = [Dreadnaught(), Fighter(), Fighter(), Fighter(), Fighter(), Carrier()]
         active_combat = True
         while active_combat:
-            fleet_1_hits = 0
-            for ship in fleet_1:
-                res = roll_d10()
-                if res >= ship.combat:
-                    fleet_1_hits += 1
-            
-
-            fleet_2_hits = 0
-            for ship in fleet_2:
-                res = roll_d10()
-                if res >= ship.combat:
-                    fleet_2_hits += 1
-                    
-            for _ in range(fleet_1_hits):
-                if len(fleet_2) == 1:
-                    break
-                i = random.choice(range(len(fleet_2)))
-                ship_to_hit = fleet_2[i]
-                ship_to_hit.take_damage()
-                if ship_to_hit.hp <= 0:
-                    fleet_2.pop(i)
-            
-            for _ in range(fleet_2_hits):
-                if len(fleet_1) == 0:
-                    break
-                i = random.choice(range(len(fleet_1)))
-                ship_to_hit = fleet_1[i]
-                ship_to_hit.take_damage()
-                if ship_to_hit.hp <= 0:
-                    fleet_1.pop(i)
+            fleet_1_hits = roll_fleet_hits(fleet_1)
+            fleet_2_hits = roll_fleet_hits(fleet_2)
+            assign_fleet_hits(fleet_1_hits, fleet_2)
+            assign_fleet_hits(fleet_2_hits, fleet_1)
             
             if len(fleet_1) == 0 and len(fleet_2) == 0:
                 print("Both fleets destroyed in the same round! DRAW")
+                draws += 1
                 active_combat = False
             elif len(fleet_1) == 0:
-                # print(f"fleet_1={fleet_1}, fleet_2={fleet_2}")
-                print("Fleet 2 wins!")
                 fleet_2_wins += 1
                 active_combat = False
             elif len(fleet_2) == 0:
-                print("Fleet 1 wins!")
                 fleet_1_wins += 1
                 active_combat = False
     
-    print(f"Fleet 1 won {fleet_1_wins} times")
-    print(f"Fleet 2 won {fleet_2_wins} times")
+    print(f"Fleet 1 won {fleet_1_wins} times ({round((fleet_1_wins/simulations) * 100)}%)")
+    print(f"Fleet 2 won {fleet_2_wins} times ({round((fleet_2_wins/simulations) * 100)}%)")
+    print(f"Draws={draws} ({round((draws/simulations) * 100)})%")
+
 
 if __name__ == "__main__":
     main()
