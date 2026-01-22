1b1bf587da6decbc4de584b88d3f64926c46009f
Brandon Tupou <tupoubrandon@gmail.com>
2026-01-22 02:55:12 +0000

    Parametrize test with different fleets

--- tests/test_fleet_sort.py
+++ tests/test_fleet_sort.py
@@ -1,7 +1,30 @@
-from src.main import sort_fleet, Dreadnaught
+from src.main import sort_fleet, Dreadnaught, Fighter, Carrier, Ship
+
+import pytest
 
 
-def test_sort():
-    fleet = [Dreadnaught(hp=1), Dreadnaught(hp=2)]
+
+@pytest.mark.parametrize(
+    ("fleet", "expected_ship", "expected_hp"),
+    [
+        (
+            [Dreadnaught(hp=1), Dreadnaught(hp=2), Dreadnaught(hp=3)],
+            Dreadnaught,
+            3
+        ),
+        (
+            [Dreadnaught(hp=1), Dreadnaught(hp=2), Fighter(), Fighter(), Carrier()],
+            Carrier,
+            1
+        ),
+    ]
+)
+def test_sort_fleet(fleet: list[Ship], expected_ship, expected_hp):
+    """
+    When a homogenous fleet is sorted, the ship with the high HP should be
+    put first.
+    """
     sort_fleet(fleet)
-    assert fleet[0].hp == 2
+
+    assert isinstance(fleet[0], expected_ship)
+    assert fleet[0].hp == expected_hp
